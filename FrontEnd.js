50b0c085273e655ef4d000d39f00ac9eed8f4297
Pradumya Gaurav <122688138+KitsuneWho@users.noreply.github.com>
2023-02-05 08:03:46 +0530

    Delete FrontEnd.js

--- FrontEnd.js
+++
@@ -1,6 +0,0 @@
-let form = document.querySelector("form");
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    let searchValue = form.elements.search.value;
-    window.location.href = `https://www.google.com/search?q=${searchValue}`;
-});
